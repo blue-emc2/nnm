e4a617cfe7776bbf78c00bd47a0c44a86df233d8
blue-emc2 <s.yamazaki@everyleaf.com>
2024-11-21 23:50:30 +0900

    Atomはcontentにも本文が入っていることがあるので取得できるように対応した
    半角スペースを置換してしまうと英文が読めないので置換をやめた

--- src/app/parser.rs
+++ src/app/parser.rs
@@ -53,10 +53,11 @@ impl Parser {
                 let atom: Atom = quick_xml::de::from_str(&body).unwrap();
                 atom.entry.iter().for_each(|item| {
                     let mut entity = Entity::new(EntityType::Atom);
+                    let description = item.summary.as_ref().or_else(|| item.content.as_ref());
                     entity.set_fields(
                         item.title.clone().unwrap_or_else(|| "".to_string()),
                         item.link.clone(),
-                        self.clean_string(item.summary.as_ref()),
+                        self.clean_string(description),
                         item.pub_date.clone(),
                     );
                     buf.push(entity);
@@ -106,7 +107,7 @@ impl Parser {
             .replace("\n", "")
             .replace("\r", "")
             .replace("\t", "")
-            .replace(" ", "");
+            .replace("&nbsp;", " ");
         tmp
     }
 }
@@ -212,6 +213,43 @@ mod tests {
                     <title>Example title 2</title>
                     <link href="https://example2.com"/>
                     <summary>Example description 2</summary>
+                    <pubDate>2024-05-24T18:00:08+09:00</pubDate>
+                </entry>
+            </feed>
+        "#
+        .to_string();
+
+        let result = parser.parse(body).unwrap();
+        assert!(result.len() == 2);
+
+        assert_eq!(result.get(0).unwrap().title, "Example title 1");
+        assert_eq!(result.get(0).unwrap().link, "https://example1.com");
+        assert_eq!(result.get(0).unwrap().description, "Example description 1");
+
+        assert_eq!(result.get(1).unwrap().title, "Example title 2");
+        assert_eq!(result.get(1).unwrap().link, "https://example2.com");
+        assert_eq!(result.get(1).unwrap().description, "Example description 2");
+    }
+
+    #[test]
+    fn test_parse_atom_content_instead_of_summary() {
+        let parser = Parser::new();
+        let body = r#"
+            <?xml version="1.0" encoding="UTF-8"?>
+            <feed xmlns="http://www.w3.org/2005/Atom">
+                <title>Example title</title>
+                <link href="https://example.com"/>
+                <updated>2024-05-24T18:00:08+09:00</updated>
+                <entry>
+                    <title>Example title 1</title>
+                    <link href="https://example1.com"/>
+                    <content>Example description 1</content>
+                    <pubDate>2024-05-24T18:00:08+09:00</pubDate>
+                </entry>
+                <entry>
+                    <title>Example title 2</title>
+                    <link href="https://example2.com"/>
+                    <content>Example description 2</content>
                     <pubDate>2024-05-24T18:00:08+09:00</pubDate>
                 </entry>
             </feed>
